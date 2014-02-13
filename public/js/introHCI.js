737bec80705d63c9e9ca54ce93ba77e9f5f5230b
Joseph Yao <jayao@ucsd.edu>
2014-02-13 10:44:50 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,19 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, addProject);
+
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	var projectHTML = '<a href = "#"" class = "thumbnail">' + 
+		'<img src="' + result['image'] + '"class=detailsImage">' +
+		'<p>' + result ['title'] + '</p>' + 
+		'<p><small>' + result['date'] + '</small></p></a>' +
+		'<div class = "details">' + result['summary'] + '</div>';
+	$('#project'+result['id']).html(projectHTML);
 }
 
 /*
@@ -34,5 +46,16 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", randomize);
 	console.log("User clicked on color button");
+}
+
+function randomize(result) {
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
